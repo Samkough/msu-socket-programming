885f39640db842293de4e382cc0585b994df94b4
Samkough <sammysamkough@gmail.com>
2018-11-16 01:36:33 -0500

    Almost working, just needs to be able to (a) have multiple clients (b) not crash if no clients are present

--- project-02/TCPNonPersistentMathClient.py
+++ project-02/TCPNonPersistentMathClient.py
@@ -6,11 +6,31 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 sentence = ""
 
-while sentence != "quit":
-    sentence = input("Input a lowercase sentence: ")
-    clientSocket.send(sentence.encode())
-    modifiedSentence = clientSocket.recv(1024)
-    print(modifiedSentence.decode())
+while True:
+    num1 = input("Input first number: ")
+    if num1 == "quit":
+        clientSocket.send(num1.encode())
+        break
+    
+    num2 = input("Input second number: ")
+    if num2 == "quit":
+        clientSocket.send(num1.encode())
+        clientSocket.send(num2.encode())
+        break
+    
+    expression = input("Input math expression: ")
+    if expression == "quit":
+        clientSocket.send(num1.encode())
+        clientSocket.send(num2.encode())
+        clientSocket.send(expression.encode())
+        break
+    
+    clientSocket.send(num1.encode())
+    clientSocket.send(num2.encode())
+    clientSocket.send(expression.encode())
+
+    result = clientSocket.recv(1024)
+    print(result.decode())
 
 print("Client is closed. Bye!")
 clientSocket.close()
--- project-02/TCPNonPersistentMathServer.py
+++ project-02/TCPNonPersistentMathServer.py
@@ -6,15 +6,43 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print("The server is ready to receive.")
+option = 0
+result = 0
 
 while True:
     if count == 0:
         connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-    count += 1
-    if sentence.lower() == "quit":
-        count = 0
+    count = 1
+    
+    num1 = connectionSocket.recv(1024).decode()  
+    num2 = connectionSocket.recv(1024).decode()
+    expression = connectionSocket.recv(1024).decode()
+    
+    # pre-processing
+    expression = str(expression)
+    expression = expression.lstrip()
+    expression = expression.rstrip()
+    
+    if expression in ("+", "add", "addition"):
+        result = int(num1) + int(num2)
+        option = 1
+    elif expression in ("-", "subtract", "subtraction"):
+        result = int(num1) - int(num2)
+        option = 2
+    elif expression in ("*", "multiply", "multiplication"):
+        result = int(num1) * int(num2)
+        option = 3
+    elif expression in ("/", "divide", "division"):
+        result = int(num1) / int(num2)
+        option = 4
+    else:
+        print("Didn't understand your expression...please try again.")
+        resultString = "Try again!"
+        connectionSocket.send(resultString.encode())
+        continue
 
+    print("Expression #" + str(count) + " Message to be sent back: " + str(result))
+    connectionSocket.send(str(result).encode())
+
+print("Server is closed. Bye!")
 connectionSocket.close()
