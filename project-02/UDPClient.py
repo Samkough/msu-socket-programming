35e1893e1918a912db08cc13399b1b3ed744ff3f
Samkough <sammysamkough@gmail.com>
2018-11-16 00:48:46 -0500

    Finished and polished

--- project-02/UDPClient.py
+++ project-02/UDPClient.py
@@ -3,11 +3,19 @@ from socket import *
 serverName = 'localhost'
 serverPort = 5000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = ""
+num1 = 0
+num2 = 0
+expression = ""
 
-while message != "quit":
-    message = input("Input a lowercase sentence: ")
-    clientSocket.sendto(message.encode(), (serverName, serverPort))
+while (num1 or num2) != "quit":
+    num1 = input("Input first number: ")
+    num2 = input("Input second number: ")
+    expression = input("Input math expression: ")
+    clientSocket.sendto(num1.encode(), (serverName, serverPort))
+    clientSocket.sendto(num2.encode(), (serverName, serverPort))
+    clientSocket.sendto(expression.encode(), (serverName, serverPort))
+    if expression == "quit":
+        break
     modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
     print(modifiedMessage.decode())
 
--- project-02/UDPServer.py
+++ project-02/UDPServer.py
@@ -4,10 +4,43 @@ serverPort = 5000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive.")
+count = 0
+result = 0
+
+def pre_process_string(string):
+    string = str(string)
+    string = string.strip()
+    print("Expression: " + string)
 
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    print("message to be sent back: ")
-    print(modifiedMessage)
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    num1, clientAddress = serverSocket.recvfrom(2048)
+    num2, clientAddress = serverSocket.recvfrom(2048)
+    expression, clientAddress = serverSocket.recvfrom(2048)
+    expression = expression.decode()
+    pre_process_string(expression)
+    
+    if expression in ("+", "add", "addition"):
+        result = int(num1) + int(num2)
+        count = 1
+    elif expression in ("-", "subtract", "subtraction"):
+        result = int(num1) - int(num2)
+        count = 2
+    elif expression in ("*", "multiply", "multiplication"):
+        result = int(num1) * int(num2)
+        count = 3
+    elif expression in ("/", "divide", "division"):
+        result = int(num1) / int(num2)
+        count = 4
+    elif expression == "quit":
+        break
+    else:
+        print("Didn't understand your expression...please try again.")
+        resultString = "Try again!"
+        serverSocket.sendto(resultString.encode(), clientAddress)
+        continue
+
+    print("Expression #" + str(count) + " Message to be sent back: " + str(result))
+    serverSocket.sendto(str(result).encode(), clientAddress)
+
+print("Server is closed. Bye!")
+serverSocket.close()
